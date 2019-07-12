81bbabd717b3b1c641d2c3571ed9f8354d05bd71
Andrew Brooking <atbrooking.dev@gmail.com>
2019-07-11 20:50:50 -0500

    Randomized question's capital

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,14 +1,18 @@
 $(document).ready(function() {
     var currentState;
+    var questionCapital;
 
     function pickState() {
         currentState = states[Math.floor(Math.random() * states.length)];
+
+        if (flip <= 0.49) {
+            questionCapital = currentState.capital;
+        } else {
+            questionCapital = states[Math.floor(Math.random() * states.length)].capital;
+        }
     }
 
-
-function displayQuestion(){
-        $('#question').text('is ' + currentCapital + ' the capital for' + currentState)
-        }
+    function displayQuestion(){
+        $('#question').text('is ' + questionCapital + ' the capital for' + currentState.state);
+    }
 });
-
-
