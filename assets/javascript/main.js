a432a144395e28571a46a0f815c9bb0791963cf1
Tahsin Nazmee <tnazmee@yahoo.com>
2019-07-11 20:38:19 -0500

    added a button function

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -5,3 +5,12 @@ $(document).ready(function() {
         currentState = states[Math.floor(Math.random() * states.length)];
     }
 });
+
+$("button").on("click", function (){
+    if ($(this).attr("id") === "true") {
+        
+    }
+    else if ($(this).attr("id") === "false") {
+
+    }
+})
