06aef83e2981dfd872043df6e3867d89755749b4
Tahsin Nazmee <tnazmee@yahoo.com>
2019-07-11 20:46:24 -0500

    moved the button function

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,24 +4,19 @@ $(document).ready(function() {
     function pickState() {
         currentState = states[Math.floor(Math.random() * states.length)];
     }
-<<<<<<< HEAD
-});
-
-$("button").on("click", function (){
-    if ($(this).attr("id") === "true") {
-        
-    }
-    else if ($(this).attr("id") === "false") {
-
-    }
-})
-=======
 
 
 function displayQuestion(){
         $('#question').text('is ' + currentCapital + ' the capital for' + currentState)
         }
+
+        $("button").on("click", function (){
+            if ($(this).attr("id") === "true") {
+                
+            }
+            else if ($(this).attr("id") === "false") {
+        
+            }
 });
 
 
->>>>>>> d68aeff9b4d987dc9c2b8f76ee6ee00133eb0338
