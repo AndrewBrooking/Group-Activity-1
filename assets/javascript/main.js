9528e9328f177dea06159a2f1e3f7f67606500a7
Andrew Brooking <atbrooking.dev@gmail.com>
2019-07-11 20:30:54 -0500

    Added IDs to tags

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
     var currentState;
 
     function pickState() {
-        currentState = 
+        currentState = states[Math.floor(Math.random() * states.length)];
     }
 });
